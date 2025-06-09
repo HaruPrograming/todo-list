f9568962c8a5935769b94ba0e98d41b2d7cac94e
HaruPrograming <haruprograming@gmail.com>
2025-06-09 21:26:26 +0900

    firebase.js編集

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.database/seeders
+     * Seed the application's database.
      */
     public function run(): void
     {
